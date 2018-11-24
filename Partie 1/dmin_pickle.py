d2f603bdc42892be48b280cbd68a3f811558356d
shipanda01 <hien.minh1996@gmail.com>
2018-11-24 11:11:43 +0100

    Perfected preprocessing and pickle

--- Partie 1/dmin_pickle.py
+++ Partie 1/dmin_pickle.py
@@ -5,34 +5,63 @@ import time
 import preprocessing as pre
 import pickle
 
+
+print("dmin.py: starting program")
+
 # ---------------------------------
 # Loading the test dataset saved by pickle. 
 # The train dataset is already processed and will be loaded later 
 # time taken: approx. 2s
-print("dmin.py: starting program\ndmin.py: loading train data")
-
-# Loading the data (saved by pickle)
-test_data = pickle.load(open("pre_test_mat.pck", "rb"))
-
+def loadTestDataFromPickle(test_filename):
+	print("-loadTestPickle: loading test data")
+	test_data = pickle.load(open(test_filename, "rb"))
+	return test_data
 
 
 # ---------------------------------
 # Applying pre-processing to the data and saving it into a pickle file
-# Uncomment this to run the pre-processing
+# We are only saving the test data, as the train data is going to be saved into
+# classes (much lighter pickle files)
+def applyPreprocessing(test_filename, filter1, filter2, imageLimit):
 
-# train_data = loadmat('train_32x32.mat')
-# pre.imageProcessingTwoFilters(train_data, pre.brightness, pre.contrast, 6000, 80)
+	test_data = loadmat('test_32x32.mat')
+	pickle.dump(pre.imageProcessingTwoFilters(test_data, filter1, filter2, imageLimit), open(test_filename, "wb"))
 
-# train_data = loadmat('test_32x32.mat')
-# pickle.dump(pre.imageProcessingTwoFilters(test_data, pre.brightness, pre.contrast, 6000, 80), open("pre_test_mat.pck", "wb"))
+	train_data = loadmat('train_32x32.mat')
+	pre.imageProcessingTwoFilters(train_data, filter1, filter2, imageLimit)
 
+	return test_data, train_data
 
 
 # ---------------------------------
-#learning function : loads the model (see dmin.py for more information)
-def averageLearningVector():
-	print("\n---averageLearningVector: loading the model from pickle")
-	return pickle.load(open("pre_train_class.pck", "rb"))
+# learning function : creates the classes from the train data
+# saves the classes into a pck file
+def saveAverageLearningVector(train_data, train_filename, imageLimit):
+	print("\n---averageLearningVector: creating the model")
+
+	avgVector = {}
+	allClassVectors = [[] for i in range(10)] #create 10 lists for the 10 classes
+
+	# putting the images into their own class depending on their label
+	# time taken: approx. 0.35s 
+	for i in range(imageLimit):
+		allClassVectors[train_data['y'][i]-1].append(train_data['X'][:, :, :, i])
+
+	# computing the average of the vectors
+	# time taken: approx. 41s. This part takes the longest
+	for i in range(10):
+		if len(allClassVectors[i]) != 0:
+			avgVector[i+1] = np.average(allClassVectors[i], axis=0)
+
+	# saving the classes
+	pickle.dump(avgVector, open(train_filename, "wb"))
+
+
+# ---------------------------------
+# learning function : loads the model (see dmin.py for more information)
+def loadAverageLearningVector(train_filename):
+	print("\n---averageLearningVector: loading the model's classes from pickle")
+	return pickle.load(open(train_filename, "rb"))
 
 
 
@@ -52,15 +81,14 @@ def findLabel(picture, averageLearningVector):
 
 # ---------------------------------
 # main classifier function
-def minimumDistanceClassifier(test):
+def minimumDistanceClassifier(avgVector, test, imageLimit):
 
 	print("\n-minimumDistanceClassifier: start")
 
 	success = 0
-	avgVector = averageLearningVector()
 	print("--findLabel: finding the distance between the data and the model")
 
-	for i in range(len(test["y"])):
+	for i in range(imageLimit):
 		label = findLabel(test["X"][:, :, :, i], avgVector)
 		if label == test["y"][i]:
 			success += 1
@@ -80,15 +108,56 @@ if __name__ == "__main__":
 	
 	start = time.time()
 
-	success = minimumDistanceClassifier(test_data)
-	successPercentage =  100.*success/len(test_data["y"])
+	# **********
+	# program settings
+	imageLimit = 500 # can be = len(train_data['y']) to process ALL the data
 
-	print("\ndmin.py: Success rate : " + str(success) + " / " 
-		+ str(len(test_data["y"])) + 
-		" (" + str(successPercentage) + "%)")
+	computePreprocessing = True
+	
+	train_filename = "train_pre_500.pck"
+	test_filename = "test_pre_500.pck"
+
+	filter1 = pre.highPassFilter
+	filter2 = pre.contrast
+	# **********
+
+
+
+	# **********
+	# this part creates the models and loads the necessary files
+	if(computePreprocessing):
+		test_data, train_data = applyPreprocessing(test_filename, filter1, filter2, imageLimit)
+		saveAverageLearningVector(train_data, train_filename, imageLimit)
+
+	test_data = loadTestDataFromPickle(test_filename) 
+	avgVector = loadAverageLearningVector(train_filename)
+
+	# **********
+
+
+
+	# **********
+	# classify the test data
+
+	if len(test_data["y"]) > imageLimit:
+		success = minimumDistanceClassifier(avgVector, test_data, imageLimit)
+		successPercentage =  100.*success/imageLimit
+
+		#display the success rate
+		print("\ndmin.py: Success rate : " + str(success) + " / " 
+			+ str(imageLimit) + 
+			" (" + str(successPercentage) + "%)")
+	else:
+		success = minimumDistanceClassifier(avgVector, test_data, len(test_data["y"]))
+		successPercentage =  100.*success/len(test_data["y"]) 
+
+		#display the success rate
+		print("\ndmin.py: Success rate : " + str(success) + " / " 
+			+ str(len(test_data["y"])) + 
+			" (" + str(successPercentage) + "%)")
 
 	end = time.time()
 
 	total = end - start
 	print("\ndmin.py: Time taken: " + str(total) + " sec.\n\n")
-	
+	# **********
